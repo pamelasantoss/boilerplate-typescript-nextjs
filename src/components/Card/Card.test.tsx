0e486dc29fb52d9621e898637147c57ff2786c57
Pamela Santos <pamesantoss@hotmail.com>
2023-07-06 16:25:09 -0300

    feat: add jest and testing react library for unit tests

---
+++ src/components/Card/Card.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import Card from './Card';
+
+describe('<Card />', () => {
+  it('should render with default props', () => {
+    render(<Card link='link.html' title='Title Example' description='Description example' />);
+
+    const linkCard = screen.getByRole('link');
+    const headingCard = screen.getByRole('heading', { name: /Title Example/i });
+    const descriptionCard = screen.getByText('Description example');
+
+    expect(linkCard).toHaveAttribute('href', 'link.html');
+    expect(headingCard).toBeInTheDocument();
+    expect(descriptionCard).toBeInTheDocument();
+  });
+});
